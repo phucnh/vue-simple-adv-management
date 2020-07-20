3da3045eb21c85b95dbd9d7ac958a4726fbbda53
Nguyen Hong Phuc <nguyen_phuc@cyberagent.co.jp>
2020-07-20 23:34:36 +0900

    Initial

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-layout row justify-center class="ma-5">
+          <v-flex xs12 sm8>
+            <v-toolbar color="blue draken-4" dark>
+              <v-app-bar-nav-icon></v-app-bar-nav-icon>
+              <v-toolbar-title class="headline">Advertiser Manage App</v-toolbar-title>
+            </v-toolbar>
+
+            <AdvertiserList />
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import AdvertiserList from '@/components/AdvertiserList.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    AdvertiserList,
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
---
+++ src/components/AdvertiserList.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-container>
+    <v-spacer />
+
+    <!-- <v-flex xs12>
+      <v-text-field clearable label="Advertiser Name" prepend-icon="mdi-filter-outline" v-bind:value="filterText" />
+    </v-flex> -->
+
+    <v-flex xs12>
+      <v-list>
+        <v-subheader class="headline">Adveritsers</v-subheader>
+
+        <v-spacer />
+
+        <v-list-item v-for="adv in advertisers" :key="adv.name">
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="adv.name"></v-list-item-title>
+          </v-list-item-content>
+          <!-- <v-btn color="red" dark icon @click="removeAdvertiser(index)"><v-icon>mdi-delete</v-icon></v-btn> -->
+        </v-list-item>
+      </v-list>
+    </v-flex>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+interface Advertiser {
+  name: string;
+}
+
+@Component
+export default class AdvertiserList extends Vue {
+  readonly name = 'AdvertiserList';
+
+  advertisers: Advertiser[] = [];
+  // [
+  //   { name: 'Advertiser 1' },
+  //   { name: 'Advertiser 2' },
+  //   { name: 'Advertiser 3' },
+  //   { name: 'Advertiser 4' },
+  //   { name: 'Advertiser 5' },
+  //   { name: 'Advertiser 6' },
+  // ];
+
+  // filterText = 'advertiser 2';
+
+  // computed properties
+  // get filteredAdvertisers(): Advertiser[] {
+  // TODO: write filter logic
+  // }
+
+  // removeAdvertiser(index: number) {
+  // TODO: write remove logic
+  // }
+}
+</script>
